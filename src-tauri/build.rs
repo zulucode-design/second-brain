cfa01f3456e009f4a55768be01b910e1705afaf9
Nicolas Zuloaga <3dbakedgoods@gmail.com>
2026-08-31 13:47:03 -0500

    fix(tests): embed Windows manifest from XML input

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,20 +1,31 @@
 #[cfg(target_os = "windows")]
 fn link_windows_common_controls() {
-    let response_path = std::path::PathBuf::from(
+    let manifest_path = std::path::PathBuf::from(
         std::env::var_os("OUT_DIR").expect("Cargo did not provide an OUT_DIR"),
     )
-    .join("common-controls-v6.rsp");
+    .join("common-controls-v6.manifest");
     std::fs::write(
-        &response_path,
+        &manifest_path,
         concat!(
-            "/MANIFEST:EMBED\n",
-            "/MANIFESTDEPENDENCY:\"type='win32' name='Microsoft.Windows.Common-Controls' ",
-            "version='6.0.0.0' processorArchitecture='*' ",
-            "publicKeyToken='6595b64144ccf1df' language='*'\"\n",
+            "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n",
+            "<assembly xmlns=\"urn:schemas-microsoft-com:asm.v1\" manifestVersion=\"1.0\">\n",
+            "  <dependency>\n",
+            "    <dependentAssembly>\n",
+            "      <assemblyIdentity type=\"win32\" ",
+            "name=\"Microsoft.Windows.Common-Controls\" version=\"6.0.0.0\" ",
+            "processorArchitecture=\"*\" publicKeyToken=\"6595b64144ccf1df\" ",
+            "language=\"*\" />\n",
+            "    </dependentAssembly>\n",
+            "  </dependency>\n",
+            "</assembly>\n",
         ),
     )
-    .expect("Could not write the Windows linker response file");
-    println!("cargo::rustc-link-arg=@{}", response_path.display());
+    .expect("Could not write the Windows Common Controls manifest");
+    println!("cargo::rustc-link-arg=/MANIFEST:EMBED");
+    println!(
+        "cargo::rustc-link-arg=/MANIFESTINPUT:{}",
+        manifest_path.display()
+    );
 }
 
 fn main() {
