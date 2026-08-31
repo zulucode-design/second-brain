0648bd4e4211b4da8b1a422aed12e50c6710bc42
Nicolas Zuloaga <3dbakedgoods@gmail.com>
2026-08-31 13:34:57 -0500

    fix(tests): embed Windows common controls manifest

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,8 @@
 fn main() {
+    #[cfg(target_os = "windows")]
+    println!(
+        "cargo::rustc-link-arg-tests=/MANIFESTDEPENDENCY:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\""
+    );
+
     tauri_build::build()
 }
