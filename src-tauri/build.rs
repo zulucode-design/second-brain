9d7532e0d64e444775807812dc62d41a1c56e951
Nicolas Zuloaga <3dbakedgoods@gmail.com>
2026-08-31 13:41:24 -0500

    fix(tests): preserve Windows manifest linker quoting

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,25 @@
+#[cfg(target_os = "windows")]
+fn link_windows_common_controls() {
+    let response_path = std::path::PathBuf::from(
+        std::env::var_os("OUT_DIR").expect("Cargo did not provide an OUT_DIR"),
+    )
+    .join("common-controls-v6.rsp");
+    std::fs::write(
+        &response_path,
+        concat!(
+            "/MANIFEST:EMBED\n",
+            "/MANIFESTDEPENDENCY:\"type='win32' name='Microsoft.Windows.Common-Controls' ",
+            "version='6.0.0.0' processorArchitecture='*' ",
+            "publicKeyToken='6595b64144ccf1df' language='*'\"\n",
+        ),
+    )
+    .expect("Could not write the Windows linker response file");
+    println!("cargo::rustc-link-arg=@{}", response_path.display());
+}
+
 fn main() {
     #[cfg(target_os = "windows")]
-    println!(
-        "cargo::rustc-link-arg=/MANIFESTDEPENDENCY:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\""
-    );
+    link_windows_common_controls();
 
     tauri_build::build()
 }
