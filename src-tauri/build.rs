7ff7ec4fdab5cf2bdceb043aad693cad7990d7f1
Nicolas Zuloaga <3dbakedgoods@gmail.com>
2026-08-31 13:37:25 -0500

    fix(tests): cover Windows unit-test harness

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     #[cfg(target_os = "windows")]
     println!(
-        "cargo::rustc-link-arg-tests=/MANIFESTDEPENDENCY:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\""
+        "cargo::rustc-link-arg=/MANIFESTDEPENDENCY:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\""
     );
 
     tauri_build::build()
